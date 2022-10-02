a2cef128abfb5526890ef62f57e4124ef6443520
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2022-10-02 15:04:23 +0600

    Update seg_tree_max.cpp

--- Data Structure/seg_tree_max.cpp
+++ Data Structure/seg_tree_max.cpp
@@ -58,7 +58,7 @@ public:
 };
 
 int main() {
-  // ios_base::sync_with_stdio(false); cin.tie(0);
+  ios_base::sync_with_stdio(false); cin.tie(0);
   int test = 1, tc = 0;
   while (test--) {
     int n; cin >> n;
