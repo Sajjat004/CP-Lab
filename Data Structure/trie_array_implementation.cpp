f469a11faae51a75edfb7a96f13d239661d806de
Sajjat004 <sajjat18cse004@gmai.com>
2024-03-23 22:37:01 +0600

    update on 23032024 v1

--- Data Structure/trie_array_implementation.cpp
+++ Data Structure/trie_array_implementation.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N   = (int) 2e5 + 5;
-#define nl      '\n'
-
 class Trie {
+  const int N = (int) 2e3 + 5;
   vector<vector<int>> node;
   vector<int> cntPath;
   int state = 0;
@@ -37,19 +35,20 @@ public:
 int main() {
   // freopen("input.txt", "r", stdin);
   ios_base::sync_with_stdio(false); cin.tie(0);
-  int test = 1, tc = 0;
-  while (test--) {
-    int n; cin >> n;
-    Trie trie;
-    for (int i = 1; i <= n; ++i) {
-      string s; cin >> s;
-      trie.add(s);
-    }
-    int q; cin >> q;
-    while (q--) {
-      string s; cin >> s;
-      cout << trie.search(s) << nl;
-    }
+  int n; cin >> n;
+  Trie trie;
+  for (int i = 1; i <= n; ++i) {
+    string s; cin >> s;
+    trie.add(s);
   }
+  string t; cin >> t;
+  cout << trie.search(t) << '\n';
+
   return 0;
 }
+
+/*
+Time complexity: O(N * avgL)
+N = number of strings
+avgL = average length of 'N' strings
+*/
--- Data Structure/trie_linked_list_implementation.cpp
+++ Data Structure/trie_linked_list_implementation.cpp
@@ -1,22 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll  = long long;
-using vll = vector<ll>;
-using vi  = vector<int>;
-using pll = pair<ll, ll>;
-using pii = pair<int, int>;
-
-const int N   = (int) 2e5 + 5;
-const int MOD = (int) 1e9 + 7;
-const int INF = (int) 1e9 + 9;
-
-#define nl      '\n'
-#define ff      first
-#define ss      second
-#define szof(x) (int) x.size()
-#define all(x)  x.begin(), x.end()
-
 class Trie {
   struct Node {
     Node* next[26];
@@ -34,8 +18,8 @@ public:
   }
   void add(string& s) {
     Node* cur = root;
-    for (int i = 0; i < (int) s.size(); ++i) {
-      int id = s[i] - 'a';
+    for (char x : s) {
+      int id = x - 'a';
       if (cur->next[id] == NULL) {
         cur->next[id] = new Node();
       }
@@ -45,8 +29,8 @@ public:
   }
   int search(string& s) {
     Node* cur = root;
-    for (int i = 0; i < szof(s); ++i) {
-      int id = s[i] - 'a';
+    for (char x : s) {
+      int id = x - 'a';
       if (cur->next[id] == NULL) return 0;
       cur = cur->next[id];
     }
@@ -57,19 +41,19 @@ public:
 int main() {
   // freopen("input.txt", "r", stdin);
   ios_base::sync_with_stdio(false); cin.tie(0);
-  int test = 1, tc = 0;
-  while (test--) {
-    int n; cin >> n;
-    Trie trie;
-    for (int i = 1; i <= n; ++i) {
-      string s; cin >> s;
-      trie.add(s);
-    }
-    int q; cin >> q;
-    while (q--) {
-      string s; cin >> s;
-      cout << trie.search(s) << nl;
-    }
+  int n; cin >> n;
+  Trie trie;
+  for (int i = 1; i <= n; ++i) {
+    string s; cin >> s;
+    trie.add(s);
   }
+  string t; cin >> t;
+  cout << trie.search(t) << '\n';
   return 0;
 }
+
+/*
+Time complexity: O(N * avgL)
+N = number of strings
+avgL = average length of 'N' strings
+*/
