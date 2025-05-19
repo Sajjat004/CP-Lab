00dda88553285a6f12c8152a127a231f1a42c9f0
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2025-05-19 18:24:04 +0600

    Update merge_sort_tree.cpp

--- Data Structure/merge_sort_tree.cpp
+++ Data Structure/merge_sort_tree.cpp
@@ -1,86 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = (int) 2e5 + 5;
-int nums[N];
-
 class MergeSortTree {
- private:
   vector<vector<int>> tree;
   int n;
 
-  void merge(vector<int>& root, vector<int>& left, vector<int>& right) {
-    int leftsz = (int) left.size(), rightsz = (int) right.size();
-    int idxl = 0, idxr = 0;
-    while (idxl < leftsz and idxr < rightsz) {
-      if (left[idxl] <= right[idxr]) { 
-        root.push_back(left[idxl]);
-        idxl++;
-      } else {
-        root.push_back(right[idxr]);
-        idxr++;
-      }
+  void merge(vector<int>& res, vector<int>& left, vector<int>& right) {
+    int n1 = left.size();
+    int n2 = right.size();
+
+    int i = 0, j = 0;
+    while (i < n1 and j < n2) {
+      if (left[i] <= right[j]) res.push_back(left[i++]);
+      else res.push_back(right[j++]);
     }
-    while (idxl < leftsz) {
-      root.push_back(left[idxl]);
-      idxl++;
-    }
-    while (idxr < rightsz) {
-      root.push_back(right[idxr]);
-      idxr++;
-    }
+    while (i < n1) res.push_back(left[i++]);
+    while (j < n2) res.push_back(right[j++]);
   }
 
-  void build(int at, int b, int e) {
-    if (b == e) {
-      tree[at].push_back(nums[b]);
+  void build(int at, int begin, int end, vector<int>& v) {
+    if (begin == end) {
+      tree[at].push_back(v[begin]);
       return;
     }
-    int mid = (b + e) >> 1;
-    build(2 * at, b, mid);
-    build(2 * at + 1, mid + 1, e);
+
+    int mid = (begin + end) / 2;
+    build(2 * at, begin, mid, v);
+    build(2 * at + 1, mid + 1, end, v);
     merge(tree[at], tree[2 * at], tree[2 * at + 1]);
   }
 
-  int query(int at, int b, int e, int l, int r, int k) {
-    if (r < b or e < l) return 0;
-    if (l <= b and e <= r) {
-      int cnt = lower_bound(tree[at].begin(), tree[at].end(), k) - tree[at].begin();
-      return cnt;
+  int query(int at, int begin, int end, int left, int right, int k) {
+    if (begin > right or end < left) return 0;
+    if (begin >= left and end <= right) {
+      return tree[at].end() - upper_bound(tree[at].begin(), tree[at].end(), k);
     }
-    int mid = (b + e) >> 1;
-    int x = query(2 * at, b, mid, l, r, k);
-    int y = query(2 * at + 1, mid + 1, e, l, r, k);
-    return x + y;
+
+    int mid = (begin + end) / 2;
+    int leftAns = query(2 * at, begin, mid, left, right, k);
+    int rightAns = query(2 * at + 1, mid + 1, end, left, right, k);
+
+    return leftAns + rightAns;
   }
 
- public:
-  MergeSortTree(int n) {
-    this->n = n;
+public:
+  MergeSortTree(vector<int>& v) {
+    n = v.size();
     tree.resize(4 * n, vector<int>());
-    build(1, 1, n);
+    build(1, 0, n - 1, v);
   }
 
-  int query(int l, int r, int k) {
-    return query(1, 1, n, l, r, k);
+  int query(int left, int right, int k) {
+    return query(1, 0, n - 1, left, right, k);
   }
 };
 
-int main() {
-  // freopen("input.txt", "r", stdin);
+int32_t main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  int test = 1, tc = 0;
-  while (test--) {
-    int n; cin >> n;
-    for (int i = 1; i <= n; ++i) cin >> nums[i];
-    MergeSortTree mst(n);
-    int q; cin >> q;
-    while (q--) {
-      int l, r, k; cin >> l >> r >> k;
-      cout << mst.query(l, r, k) << '\n';
-    }
+
+  int n; cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) cin >> v[i];
+
+  MergeSortTree mst(v);
+  int q; cin >> q;
+  while (q--) {
+    int l, r, k; cin >> l >> r >> k;
+    l--; r--;
+    cout << mst.query(l, r, k) << '\n';
   }
+    
   return 0;
 }
 
-// find number of element less then k in a given range l to r.
+// Time Complexity: O(n log n + q log n), where n is the size of the array and q is the number of queries
+// Space Complexity: O(n log n), where n is the size of the array
+
+// Pboblem Link: https://www.spoj.com/problems/KQUERY/
