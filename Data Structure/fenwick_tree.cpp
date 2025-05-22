81472192332ce089620c702a800be73279e76ce6
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2025-05-22 17:56:54 +0600

    Update fenwick_tree.cpp

--- Data Structure/fenwick_tree.cpp
+++ Data Structure/fenwick_tree.cpp
@@ -5,25 +5,37 @@ template <typename type> class FenwickTree {
 private:
   vector<type> tree;
   int n;
+
 public:
   FenwickTree(int n) {
     this->n = n;
     tree.assign(n + 2, 0);
   }
-  // Time Complexity: O(logN)
+
+  FenwickTree(vector<type>& a) {
+    n = a.size();
+    tree.assign(n + 2, 0);
+    for (int i = 1; i <= n; ++i) {
+      update(i, a[i]);
+    }
+  }
+
   void update(int idx, type val) {
     for ( ; idx <= n; idx += (idx & -idx)) tree[idx] += val;
   }
-  // Time Complexity: O(logN)
+
   type query(int idx) {
     type sum = 0;
     for ( ; idx > 0; idx -= (idx & -idx)) sum += tree[idx];
     return sum;
   }
+
+  // Range update only works for single point query
   void update(int l, int r, int val) {
     update(l, val);
     update(r + 1, -val);
   }
+
   type query(int l, int r) {
     return query(r) - query(l - 1);
   }
@@ -31,23 +43,18 @@ public:
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  int test = 1, tc = 0;
-  while (test--) {
-    int n;
-    cin >> n;
-    int ar[n + 2];
-    for (int i = 1; i <= n; ++i) cin >> ar[i];
-    FenwickTree<int> ds(n);
-    for (int i = 1; i <= n; ++i) {
-      ds.update(i, ar[i]);
-    }
-    int m;
-    cin >> m;
-    while (m--) {
-      int l, r;
-      cin >> l >> r;
-      cout << ds.query(l, r) << '\n';
-    }
+
+  int n; cin >> n;
+  vector<int> a(n + 1);
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+
+  FenwickTree<int> ds(a);
+  
+  int m; cin >> m;
+  while (m--) {
+    int left, right; cin >> left >> right;
+    cout << ds.query(left, right) << '\n';
   }
+
   return 0;
 }
